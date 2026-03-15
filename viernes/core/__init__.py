58400e622707d05b093f71d5450f643baaa02a88
Tomás Vanegas <tomasvanpez@gmail.com>
2026-03-15 18:15:17 -0500

    Initial clean commit

--- viernes/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Subpaquete de núcleo de VIERNES OS.
-
-Contiene componentes de bajo nivel como fusión de sensores,
-seguimiento espacial y composición AR.
-"""
--- viernes/interaction/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Subpaquete de interacción de VIERNES OS.
-
-Incluye componentes de entrada por voz y gestos.
-"""
