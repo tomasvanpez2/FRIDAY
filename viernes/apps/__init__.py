ec5318e9f228d037bb98809e757b685b1dbffb2a
Tomás Vanegas <tomasvanpez@gmail.com>
2026-03-15 18:15:55 -0500

    01

--- viernes/apps/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Subpaquete de aplicaciones de VIERNES OS.
-
-Contiene aplicaciones de alto nivel construidas sobre la capa AR.
-"""
--- viernes/cognitive/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Subpaquete cognitivo de VIERNES OS.
-
-Agrupa el motor de lenguaje y el sistema de conocimiento.
-"""
